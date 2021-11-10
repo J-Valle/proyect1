7a53f6dfdc0ed17f52306dde5919d01203f82580
J-Valle <josuedavid.valle94@gmail.com>
2021-11-10 21:38:07 +0100

    Triangulico

---
+++ src/main/scala/Triangulo.scala
@@ -0,0 +1,22 @@
+//Pedir un numero por consola y crear una piramide de asteriscos
+//EJ: Si se introduce un 3, se crea un triangulo de 3 filas, y si se pide mas, se adapta
+//   *
+//  * *
+// * * *
+// Notas: OK, parece ser que se puede conseguir con recursividad segun Adri, para ello tengo en mente poner un valor
+// inicial a 0 y que el numero insertado sean incrementos para la siguiente linea en la cual se añada "_*" por cada
+// valor en referencia a la linea anterior, una a una, hasta que se llegue al valor puesto por el usuario
+
+import scala.io.StdIn
+
+
+object Triangulo extends App {
+ println("Define la altura del triangulico")
+  val num1 = StdIn.readInt()
+
+
+
+
+
+
+}
